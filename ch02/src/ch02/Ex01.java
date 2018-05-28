9c60eab0b7c2403db66cf8a42b1846d08de1cf3a
teachertophoon <teachertophoon@gmail.com>
2018-05-28 18:49:31 +0900

    2장 연습문제 5번까지 풀이

--- ch02/src/ch02/Ex01.java
+++ ch02/src/ch02/Ex01.java
@@ -1,21 +1,10 @@
 package ch02;
 
-import java.util.Scanner;
-
 public class Ex01 {
-	public static void main(String[] args) {
-		// 1. Scanner를 이용해서 2자리 정수를 입력받으세요.
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("두자리 정수를 하나 입력해주세요.>");
-		//String input = scanner.nextLine();
-		//int num = Integer.parseInt(input);	// 입력받은 문자열을 숫자로 변환
-		int num = scanner.nextInt();	// 입력받은 문자열을 정수(int)로 받아 전달
-		int num2 = scanner.nextInt();	// 입력받은 문자열을 정수(int)로 받아 전달
-		System.out.printf("num=%d, num2=%d%n", num, num2);
-
-		/* 2. 입력받은 정수를 printf 메소드를 이용해서
-		10진수, 8진수, 16진수로 출력하세요.*/
-		System.out.printf("10진수=%d, 8진수=%o, 16진수=%x", num, num, num);
-	}
+	/*
+	 * 논리형: boolean(1byte)
+	 * 문자형: char(2byte)
+	 * 정수형: byte(1byte), short(2byte), int(4byte), long(8byte)
+	 * 실수형: float(4byte), double(8byte)
+	 */
 }
