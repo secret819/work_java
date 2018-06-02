d02e5a2f003080e134b453a28e39ef6f8a0c2b3f
KOITT <KOITT@KOITT-PC>
2018-06-02 17:42:01 +0900

    4장 Ex11 155p 까지

--- ch03/src/ch03/Ch03Ex04_01.java
+++ ch03/src/ch03/Ch03Ex04_01.java
@@ -2,10 +2,19 @@ package ch03;
 
 public class Ch03Ex04_01 {
 	public static void main(String[] args) {
+		// 프로그램 실행 중에 1번 위치의 a값이 궁금할 경우
+		// 방법 1: print 메소드를 이용해 출력해본다.
+		// 방법 2: 디버깅 도구를 사용한다.(비파괴식 검사)
 		int a = 5;
 		a += 10;
-		a = a - 1; // ①
-		//System.out.printf(“%d%n”, a);
+		System.out.println(a);	// 방법 1: 파괴식 검사
+		a = a - 1;	// 1번
+		debugTest(a);	// F5: 메소드 내부 진입  , F6: 현재 가리키고 있는 줄 실행, F8: 다음 break point로 이동
+		System.out.printf("%d%n", a);
 	}
 
+	
+	private static void debugTest(int b) {
+		System.out.println(b + 10);
+	}
 }
