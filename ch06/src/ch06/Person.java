daecb3c7ff2673f38b82feb1769394cd2607d703
KOITT <KOITT@KOITT-PC>
2018-06-29 21:28:43 +0900

    6장 예제 29번까지

--- ch06/src/ch06/Person.java
+++ ch06/src/ch06/Person.java
@@ -9,8 +9,23 @@ class Person {
 	// 3. 주소
 	String address;
 	
+	// [2-1] 이름, 나이, 주소 인스턴스 변수를 초기화하는 생성자를 작성하시오.
+	Person (String name, int age, String address){
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+	
+	// [2-3] 이름만 입력받는 생성자를 작성하시오.
+	// 단, 나이와 주소값은 각각 기본으로 19, "없음" 값으로 초기화하는 생성자를 작성하시오.
+	Person (String name){
+		this(name, 19, "없음");
+	}
+
 	// 행동(메서드)
 	// 1. 자기소개
-	void self_introduction() { System.out.printf("저는 %d살 %s에 사는 %s입니다.", age, address, name); }
+	void introduce() { 
+		System.out.printf("저는 %d살 %s에 사는 %s입니다.", age, address, name);
+	}
 	// "저는 [나이]살 [주소]에 사는 [이름]입니다."
 }
--- ch06/src/ch06/PersonTest.java
+++ ch06/src/ch06/PersonTest.java
@@ -40,28 +40,29 @@ class PersonTest {
 		
 		// 2. 각 사람(객체)마다 이름, 나이, 주소를 설정하세요.
 				// (자기 자신 포함해서 옆사람 정보 작성하기)
-		for (int i = 0; i < p.length; i++) {
+		/*for (int i = 0; i < p.length; i++) {
 			p[i] = new Person();			
-		}
+		}*/
 		
-		p[0].name = "홍길동";
-		p[1].name = "김철수";
-		p[2].name = "이영희";
-		
-		p[0].age = 26;
-		p[1].age = 25;
-		p[2].age = 24;
-		
-		p[0].address = "신림동";
-		p[1].address = "봉천동";
-		p[2].address = "신림동";
+		// [2-2] 아래 방법은 객체를 생성 후 각 인스턴스 변수에 접근하여
+		// 값을 초기화 한 부분이다.
+		// 아래 부분을 2-1에서 작성한 생성자를 이용하여 변경하시오.
+		p[0] = new Person("철수", 40, "서울");
+		p[1] = new Person("영희", 38, "부산");
+		p[2] = new Person("길동", 19, "대전");
 		
 		
 		// 3. Person 클래스에서 만든 메서드를 각 사람(객체)마다 호출하세요.
 				// for문 이용해도 좋고, 각각 3줄로 호출해도 됩니다.
 		for (int i = 0; i < p.length; i++) {
-			p[i].self_introduction();
+			p[i].introduce();
 			System.out.println();
 		}
+		
+		// [2-4] 2-3에서 작성한 생성자를 이용하여 "흥민"이라는 이름으로 사람 객체 하나를 생성하세요.
+		Person p1 = new Person("흥민");
+		
+		// [2-5] 2-4 객체를 이용하여 introduce() 메서드를 호출하세요.
+		p1.introduce();
 	}	
 }
